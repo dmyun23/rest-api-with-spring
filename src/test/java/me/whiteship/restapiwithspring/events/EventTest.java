e86b68db8e9e50eb8a3f7ac38e7a0885f4b7941a
dmyun23 <dmyun23@gmail.com>
2022-11-12 22:12:41 +0900

    Event domain

---
+++ src/test/java/me/whiteship/restapiwithspring/events/EventTest.java
@@ -0,0 +1,30 @@
+package me.whiteship.restapiwithspring.events;
+
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EventTest {
+
+    @Test
+    public void builder() {
+        Event event = Event.builder().build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean() {
+        // Given
+        String name = "Event";
+        String description = "Spring";
+
+        // When
+        Event event = new Event();
+        event.setName(name);
+        event.setDescription(description);
+
+        // Then
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
+}
